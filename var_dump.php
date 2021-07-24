ed97a025af44a6bfa1e23d900fbc893fd1f0eec1
manisha <manisha.verma23@gmail.com>
2021-07-24 19:22:38 +0530

    var_dump()function

---
+++ var_dump.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>var_dump()function</title>
+</head>
+<body>
+
+
+<!-- The var_dump() function is used to display structured information (type and value) about one or more variables. -->
+
+    <?php
+      $x = true;
+      $y = false;
+      echo  var_dump($x);
+      echo "<br>";
+      echo  var_dump($y) ;
+
+    ?>
+</body>
+</html>
+
+    
+
